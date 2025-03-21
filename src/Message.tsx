96c2b1aadb3b684a7a02a4fc8cbad516f65110d8
duyixian <duyixian@meideng.net>
2025-03-21 14:53:48 +0800

    添加showdown库以支持Markdown渲染，更新消息组件以使用HTML内容显示

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,14 +1,20 @@
+import showdown from "showdown";
+
 export interface Message {
   role: "user" | "assistant";
   content: string;
 }
+const converter = new showdown.Converter();
 export function MessageItem(props: { message: Message }) {
   return (
     <div class={`message-item ${props.message.role}`}>
       <div class="message-header">
         <b>{props.message.role === "user" ? "用户" : "助手"}</b>
       </div>
-      <div class="message-content">{props.message.content}</div>
+      <div
+        class="message-content"
+        innerHTML={converter.makeHtml(props.message.content)}
+      />
     </div>
   );
 }
@@ -18,7 +24,10 @@ export function ReplyingMessage(props: { content: string }) {
       <div class="message-header">
         <b>助手</b>
       </div>
-      <div class="message-content">{props.content}</div>
+      <div
+        class="message-content"
+        innerHTML={converter.makeHtml(props.content)}
+      />
     </div>
   );
 }
